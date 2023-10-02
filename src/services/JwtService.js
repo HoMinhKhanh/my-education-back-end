35aacf4f101025b1cfca440ed4b03b5e5e40cb9f
khanhhm <hokhanhzxz@gmail.com>
2023-10-02 15:15:52 +0700

    Done BE Sprint 1

--- src/services/JwtService.js
+++ src/services/JwtService.js
@@ -5,7 +5,7 @@ dotenv.config()
 const generatedAccessToken = (payload) => {
     const access_token = jwt.sign({
         payload
-    }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+    }, process.env.ACCESS_TOKEN, { expiresIn: '1p' })
 
     return access_token
 }
@@ -18,7 +18,35 @@ const generatedRefreshToken = (payload) => {
     return refresh_token
 }
 
+const refreshTokenJwtService = (token) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            jwt.verify(token, process.env.REFRESH_TOKEN, async (err, user) => {
+                if (err) {
+                    resolve({
+                        status: 'error',
+                        message: 'The authentication',
+                    })
+                }
+                const { payload } = user
+                const access_token = await generatedAccessToken({
+                    id: payload?.id,
+                    role: payload?.role
+                })
+                resolve({
+                    status: 'OK',
+                    message: 'Success',
+                    access_token
+                })
+            })
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
     generatedAccessToken,
-    generatedRefreshToken
+    generatedRefreshToken,
+    refreshTokenJwtService
 }
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -103,8 +103,74 @@ const updateUser = (id, data) => {
     })
 };
 
+const deleteUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkUser = await User.findOne({
+                _id: id,
+            })
+            if (checkUser === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The user is not defined'
+                })
+            }
+
+            await User.findByIdAndDelete(id, { new: true })
+            resolve({
+                status: 'OK',
+                message: 'Delete user Success',
+            })
+        } catch (e) {
+            reject(e);
+        }
+    })
+};
+
+const getAllUser = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const allUser = await User.find()
+            resolve({
+                status: 'OK',
+                message: 'List all users',
+                data: allUser
+            })
+        } catch (e) {
+            reject(e);
+        }
+    })
+};
+
+const getDetailsUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const user = await User.findOne({
+                _id: id,
+            })
+            if (user === null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The user is not defined',
+                })
+            }
+
+            resolve({
+                status: 'OK',
+                message: 'Success',
+                data: user,
+            })
+        } catch (e) {
+            reject(e);
+        }
+    })
+};
+
 module.exports = { 
     createUser,
     loginUser,
-    updateUser
+    updateUser,
+    deleteUser,
+    getAllUser,
+    getDetailsUser,
 }
